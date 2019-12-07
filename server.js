cbf9184bb1ac94067830b6a2db4a78b982294b09
linnaek <linnaekraemer@gmail.com>
2019-12-06 22:19:10 -0600

    connect to database

--- server.js
+++ server.js
@@ -2,12 +2,18 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var session = require('express-session');
 var logger = require('morgan');
+var methodOverride = require('method-override')
+
+require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+require('./config/database')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,7 +23,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret: 'SEIRocks!',
+  resave: false,
+  saveUninitialized: true
+}));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride('_method'))
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
